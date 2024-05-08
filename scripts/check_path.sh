ab3eb290832eebfd7d18827fece1041a5ad0166e
Tommaso Cerruti <79256764+tommasocerruti@users.noreply.github.com>
2024-05-08 12:50:46 +0200

    Update check_path.sh

--- scripts/check_path.sh
+++ scripts/check_path.sh
@@ -1,3 +1,5 @@
+#script checking what paths included in $PATH do not exist
+
 for dir in $(echo $PATH | tr ':' '\n'); do
     if [ ! -d "$dir" ]; then
         echo "Directory '$dir' does not exist."
